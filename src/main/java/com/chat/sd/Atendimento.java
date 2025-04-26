e75f96d7abc667bec57c63897795c5569335bc64
gustavoo151 <josegustavooliveira121@gmail.com>
2025-04-26 13:48:47 -0300

    Add socket and dependency management to Atendimento class
    Refactor Atendimento to include socket initialization, dependency management via AtendimentoManager, and a database instance. Prepared the class for handling client communication using a buffered reader.

--- src/main/java/com/chat/sd/Atendimento.java
+++ src/main/java/com/chat/sd/Atendimento.java
@@ -1,5 +1,22 @@
 package com.chat.sd;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
 public class Atendimento {
+    private final Socket clienteSocket;
+    private final AtendimentoManager manager;
+    private BufferedReader leitorCliente;
+    private final BancoDeDados bancoDeDados;
+
+    public Atendimento(Socket clienteSOcket, AtendimentoManager manager){
+        this.clienteSocket = clienteSOcket;
+        this.manager = manager;
+        this.bancoDeDados = new BancoDeDados();
+        this.leitorCliente = new BufferedReader(new InputStreamReader(System.in));
+    }
 
 }
